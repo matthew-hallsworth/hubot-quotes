1e71eacd980aa6296ee7224ef535e00a73a57de3
Matthew Hallsworth <matthew@mhalls.net>
2014-10-01 18:46:45 +1000

    Add startup script

---
+++ jenni.sh
@@ -0,0 +1,7 @@
+export HUBOT_IRC_SERVER="irc.lolliebag.com"
+export HUBOT_IRC_PORT=6667
+export HUBOT_IRC_ROOMS="#jenni"
+export HUBOT_IRC_NICK="jenni"
+export HUBOT_IRC_UNFLOOD="true"
+
+bin/hubot -a irc --name jenni
